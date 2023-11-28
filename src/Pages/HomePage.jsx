5d270210fc74b67f98deacdf7af9dbc22ab4c289
Ajith31kumar <ajithkumar06952@gmail.com>
2023-11-28 10:15:53 +0530

    first commit

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,7 +6,7 @@ const Homepage = () => {
     <div className="d-flex flex-column ml-1 justify-content-center align-items-center min-vh-100">
       <h1 className="display-5 fw-bold lh-1 gradient-bg mb-5">Library Management System</h1>
       <p className="lead text-center gradient-bg2">
-        Designed By SHANMUGAM R
+        Designed By Ajith Kumar K
         <span className="material-symbols-outlined">deployed_code</span>
       </p>
     </div>
